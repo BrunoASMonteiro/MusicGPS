c30edd3ee1cb1a8df51f9e7644b09ba8a764513c
brumonteiro <monteirobruno057@gmail.com>
2024-02-06 22:38:55 +0000

    oh estrela que és cometa

--- app/src/main/java/com/example/gpsmusic/MainActivity.java
+++ app/src/main/java/com/example/gpsmusic/MainActivity.java
@@ -32,15 +32,12 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_LOCATION_PERMISSION = 1;
-    private static final int REQUEST_ENABLE_BT = 2;
-    private BluetoothAdapter bluetoothAdapter;
+
 
     private LocationManager locationManager;
     private LocationListener locationListener;
-    private BluetoothSocket bluetoothSocket;
-    UUID uuid;
-    String uuidString;
-    private MediaPlayer mediaPlayer;
+
+
     TextView speedView;
     Button play_btn;
     Button stop_btn;
@@ -67,13 +64,8 @@ public class MainActivity extends AppCompatActivity {
         speedView = findViewById(R.id.GPS);
         play_btn = findViewById(R.id.play);
         stop_btn = findViewById(R.id.stop);
-        uuid = UUID.randomUUID();
-
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-
         locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
 
@@ -99,45 +91,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
         };
-
-
-
-
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-        ArrayList<BluetoothDevice> deviceList = new ArrayList<>();
-        uuidString = uuid.toString();
-        mediaPlayer = new MediaPlayer();
-
-        for (BluetoothDevice device : pairedDevices) {
-            deviceList.add(device);
-        }
-
-        if (bluetoothAdapter == null) {
-            // O dispositivo não suporta Bluetooth
-            // Lide com isso de acordo com as necessidades do seu aplicativo
-        } else {
-            if (!bluetoothAdapter.isEnabled()) {
-                // O Bluetooth está desativado, solicite ao usuário que o ative
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-
-            }
-        }
-
-
         requestLocationPermission();
-
-
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -148,165 +102,26 @@ public class MainActivity extends AppCompatActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
         play_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                BluetoothDevice device = getFirstPairedDevice();
-                if (device != null) {
-                    connectToDevice(device);
-                }
+
             }
         });
         stop_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stopMusicPlayback();
+
             }
         });
 
     }
 
-    private void connectToDevice(BluetoothDevice device) {
-        try {
-            // Verificar permissão antes de estabelecer a conexão Bluetooth
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.BLUETOOTH_CONNECT}, REQUEST_ENABLE_BT);
-                return;
-            }
-
-            // Criar um socket Bluetooth seguro
-            bluetoothSocket = device.createRfcommSocketToServiceRecord(uuid);
-            bluetoothSocket.connect(); // Tentativa de conexão
-
-            // Iniciar uma thread para receber dados do Bluetooth
-            Thread receiveThread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        InputStream inputStream = bluetoothSocket.getInputStream();
-                        byte[] buffer = new byte[1024]; // Buffer para armazenar dados recebidos
-                        int bytesRead;
-
-                        // Loop para ler dados continuamente
-                        while (true) {
-                            bytesRead = inputStream.read(buffer);
-                            if (bytesRead == -1) {
-                                break; // Fim da transmissão
-                            }
-
-                            // Reproduzir os dados de música recebidos
-                            startMusicPlayback(Arrays.copyOf(buffer, bytesRead));
-                        }
-                    } catch (IOException e) {
-                        // Tratamento de erros na leitura de dados Bluetooth
-                        e.printStackTrace();
-                    } finally {
-                        // Garantir que o socket seja fechado após o término da transmissão
-                        closeBluetoothSocket();
-                    }
-                }
-            });
-            receiveThread.start(); // Iniciar a thread de recepção
-
-        } catch (IOException e) {
-            // Tratar exceções relacionadas à conexão Bluetooth
-            e.printStackTrace();
-        }
-    }
-
-    private void closeBluetoothSocket() {
-        try {
-            if (bluetoothSocket != null) {
-                bluetoothSocket.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
 
-    private void startMusicPlayback(byte[] musicData) {
-        try {
-            // Cria um arquivo temporário
-            File tempFile = File.createTempFile("tempMusic", ".tmp", getCacheDir());
-            tempFile.deleteOnExit();
 
-            // Escreve os dados no arquivo
-            try (FileOutputStream fos = new FileOutputStream(tempFile)) {
-                fos.write(musicData);
-            }
-
-            // Prepara o MediaPlayer para reproduzir o arquivo
-            mediaPlayer.reset();
-            mediaPlayer.setDataSource(tempFile.getAbsolutePath());
-            mediaPlayer.prepareAsync();
-            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    mediaPlayer.start();
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-            // Trate os erros apropriadamente
-        }
-    }
-
-
-    private void stopMusicPlayback() {
-        if (mediaPlayer.isPlaying()) {
-            mediaPlayer.stop();
-        }
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case REQUEST_LOCATION_PERMISSION:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startLocationUpdates();
-                } else {
-                    // Informar ao usuário que a permissão foi negada.
-                }
-                break;
-            case REQUEST_ENABLE_BT:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Bluetooth agora pode ser utilizado.
-                } else {
-                    // Informar ao usuário que a permissão de Bluetooth foi negada.
-                }
-                break;
-        }
-    }
-
-
-    private void startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        }
-    }
-
-    private BluetoothDevice getFirstPairedDevice() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            // Solicitar a permissão Bluetooth ao usuário
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.BLUETOOTH_CONNECT},
-                    REQUEST_ENABLE_BT); // Certifique-se de que REQUEST_ENABLE_BT é um código de solicitação constante definido em sua classe
-            return null;
-        }
-
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-        if (!pairedDevices.isEmpty()) {
-            // Retorna o primeiro dispositivo da lista de dispositivos emparelhados
-            return pairedDevices.iterator().next();
-        }
-        return null;
-    }
 
 
 
